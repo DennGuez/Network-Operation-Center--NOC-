49d1cb9ee11862d7e6ba053c8aa91cd2b0df3eba
Dennis Rodriguez <dennisrodriguezx@gmail.com>
2023-12-15 20:15:53 -0400

    FEAT: Enviamos el email de un UseCase

--- src/presentation/email/email.service.ts
+++ src/presentation/email/email.service.ts
@@ -1,10 +1,17 @@
 import nodemailer from 'nodemailer';
 import { envs } from '../../config/plugins/envs.plugin';
+import { LogRepository } from '../../domain/repository/log.repository';
+import { LogEntity, LogSeverityLevel } from '../../domain/entities/log.entity';
 interface SendEmailOptions {
-    to: string,
+    to: string | string[],
     subject: string,
     htmlBody: string,
-    // attachements:
+    attachments?: Attachment[]
+}
+
+interface Attachment {
+    filename: string;
+    path: string
 }
 
 /* Sera nuestro servicio para poder enviar correos pero tambien  / 
@@ -19,20 +26,56 @@ export class EmailService {
         }
     });
 
+
+    constructor(){}
+
     async sendEmail( options: SendEmailOptions ):Promise<boolean> {
-        const { to, subject, htmlBody } = options;
+        const { to, subject, htmlBody, attachments = [] } = options;
 
         try {
             const sendInformation = await this.transporter.sendMail({
                 to: to,
                 subject: subject,
                 html: htmlBody,
+                attachments: attachments
             });
             console.log(sendInformation);
-            
+
+    /* Si todo sale bien creamos el log para enviar al repository */
+            const log = new LogEntity({
+                level: LogSeverityLevel.low,
+                message: "Email sent",
+                origin: "email.service.ts"
+            });          
+
             return true;
         } catch (error) {
+            const log = new LogEntity({
+                level: LogSeverityLevel.high,
+                message: "Email not sent",
+                origin: "email.service.ts"
+            });          
             return false;
         }
     }
+
+    async sendEmailWithFileSystemLogs( to: string | string [] ) {
+        const subject = 'Logs del servidor'
+        const htmlBody = `
+            <h3>Logs de sistema - NOC</h3>
+            <p>Lorem impsum</p>
+            <p>Ver logs adjuntos</p>
+        `;
+        const attachments: Attachment[] = [
+            { filename: 'logs-all.log', path: './logs/logs-all.log' },
+            { filename: 'logs-high.log', path: './logs/logs-high.log' },
+            { filename: 'logs-medium.log', path: './logs/logs-medium.log' },
+        ];
+        
+        return this.sendEmail({
+            to, subject, attachments, htmlBody
+        });
+    }
+
+
 }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,3 +1,4 @@
+import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { EmailService } from "./email/email.service";
@@ -7,22 +8,35 @@ const fileSystemLogRepository = new LogRepositoryImpl(
     // new postgresSQLLogDatasource()
 );
 
+const emailService = new EmailService();
+
 export class Server {
     public static start() {
 
         console.log('Server started...');
+        
+        new SendEmailLogs(
+            emailService,
+            fileSystemLogRepository
+        ).execute(
+            ['dennisrodriguezx@gmail.com', 'holasoydennis@gmail.com']
+        )
 
         /* Mandar email */
-        const emailService = new EmailService();
-        emailService.sendEmail({
-            to: 'dennisrodriguezx@gmail.com',
-            subject: 'Logs del sistema',
-            htmlBody: `
-                <h3>Logs de sistema - NOC</h3>
-                <p>Lorem impsum</p>
-                <p>Ver logs adjuntos</p>
-            `
-        })
+        // emailService.sendEmail({
+        //     to: 'dennisrodriguezx@gmail.com',
+        //     subject: 'Logs del sistema',
+        //     htmlBody: `
+        //         <h3>Logs de sistema - NOC</h3>
+        //         <p>Lorem impsum</p>
+        //         <p>Ver logs adjuntos</p>
+        //     `
+        // })
+
+        /* Mandar email con archivos */
+        // emailService.sendEmailWithFileSystemLogs(
+        //     ['dennisrodriguezx@gmail.com']
+        // )
         
         /* Pasamos argumentos a nuestro servicio y lo ejecutamos cada 5 sec */
         // CronService.createJob(
